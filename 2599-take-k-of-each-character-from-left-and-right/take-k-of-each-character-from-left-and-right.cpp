32b6648cc75692c93bd61a7ea42d9af735346b49
nakul-grover <158311965+nakul-grover@users.noreply.github.com>
2024-05-18 15:15:31 +0530

    Time: 159 ms (15.23%) | Memory: 28.1 MB (11.78%) - LeetSync

--- 2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.cpp
+++ 2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.cpp
@@ -1,55 +1,52 @@
 class Solution {
 public:
-
-    bool satisfy(vector<int>& freq, int k){
-        if(freq[0]>=k && freq[1]>=k && freq[2]>=k) return true;
-        else return false;
+    bool isValid(vector<int>&freq,int k){
+        if(freq[0]>= k && freq[1] >=k && freq[2]>=k){
+            return true;
+        }
+        return false;
     }
-
-    bool isValid(string s, int k, int ptr){
-        vector<int> freq(3, 0);
-
-        for(int i=0; i<ptr; i++){
+    bool satisfy(string s,int key,int k){
+        vector<int>freq(3,0);
+        for(int i=0;i<key;i++){
             freq[s[i]-'a']++;
         }
-        if(satisfy(freq, k)) return true;
+        if(isValid(freq,k)){
+            return true;
+        };
+        int i = key-1;
+        int j=s.size()-1;
 
-        int size = s.length();
-        int left = ptr-1;
-        int right = size-1;
-
-        while(left>=0){
-            freq[s[left]-'a']--;
-            freq[s[right]-'a']++;
-            if(satisfy(freq, k)) return true;
-            left--;
-            right--;
-        }
-
-        return false;
-
-    }
-
-    int takeCharacters(string s, int k) {
-        
-        int start = 0;
-        int end = s.length();
-        int ans = INT_MAX;
-        while(start<=end){
-            int mid = start+(end-start)/2;
-
-            if(isValid(s, k, mid)){
-                ans = min(ans, mid);
-                end = mid-1;
-            }else{
-                start = mid+1;
+        while(i>=0){
+            freq[s[j]-'a']++;
+            freq[s[i]-'a']--;
+            j--;
+            i--;
+            if(isValid(freq,k)){
+                return true;
             }
-
         }
-
-        if(ans==INT_MAX) ans = -1;
-
+        return false;
+        
+    }
+    int takeCharacters(string s, int k) {
+        int low =0;
+        int ans=INT_MAX;
+        int high =s.size();
+        while(low<=high){
+            int mid=low+((high-low)/2);
+            if(satisfy(s,mid,k)){
+                ans=mid;
+                high = mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        if(ans==INT_MAX){
+            return -1;
+        }
         return ans;
-
+        
     }
 };
