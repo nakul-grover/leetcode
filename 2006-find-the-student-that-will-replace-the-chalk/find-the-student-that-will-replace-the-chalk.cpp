10e9b63f8e836175f5194614415db7282b1681f4
nakul-grover <158311965+nakul-grover@users.noreply.github.com>
2024-09-02 16:49:50 +0530

    Time: 728 ms (5.04%) | Memory: 77 MB (18.08%) - LeetSync

--- 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.cpp
+++ 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
     int chalkReplacer(vector<int>& chalk, int k) {
+    
      long long  int sum = 0;
-      int n= chalk.size();
+     int n= chalk.size();
 
       for(int i=0;i<n;i++){
         sum+=chalk[i];
       }
-      cout<<sum<<" ";
+
       while(k>=sum){
         k = k-sum;
       }
-      cout<<k;
+
       for(int i=0;i<n;i++){
         if(k<chalk[i]){
             return i;
         }
         k-=chalk[i];
       }
+      
       return -1;
         
     }
