e1ff1a7dc2e08d5a17e401022efaab6dadbe472d
nakul-grover <158311965+nakul-grover@users.noreply.github.com>
2024-09-03 19:53:31 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (16.57%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.cpp
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
-int lengthOfLastWord(string s) {
-int i=s.size()-1;
-while(s[i]==' ')
-i--;
-int count=0;
-for(;i>=0;i--)
-{
-if(s[i]!=' ')
-count++;
-if(i==0||s[i]==' ')
-break;
-}
-return count;
-}
+    int lengthOfLastWord(string s) {
+        int i = s.size() - 1;
+        while (s[i] == ' ')
+            i--;
+        int count = 0;
+        for (; i >= 0; i--) {
+            if (s[i] != ' ')
+                count++;
+            if (i == 0 || s[i] == ' ')
+                break;
+        }
+        return count;
+    }
 };
