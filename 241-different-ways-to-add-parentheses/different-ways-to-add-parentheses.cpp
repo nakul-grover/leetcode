2cb2d6dafc694df6429f14a9ec5640eb8ccbd364
nakul-grover <158311965+nakul-grover@users.noreply.github.com>
2024-09-19 21:48:40 +0530

    Time: 5 ms (22.74%) | Memory: 14 MB (15.20%) - LeetSync

---
+++ 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> solve(string s) {
+        vector<int> ans;
+
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '+' || s[i] == '-' || s[i] == '*') {
+                vector<int> left = solve(s.substr(0, i));
+                vector<int> right = solve(s.substr(i + 1));
+
+                for (int l : left) {
+                    for (int r : right) {
+                        if (s[i] == '+') {
+                            ans.push_back(l + r);
+                        } else if (s[i] == '-') {
+                            ans.push_back(l - r);
+                        } else {
+                            ans.push_back(l * r);
+                        }
+                    }
+                }
+            }
+        }
+
+        if (ans.empty()) {
+            ans.push_back(stoi(s));  
+        }
+
+        return ans;
+    }
+
+    vector<int> diffWaysToCompute(string expression) {
+        return solve(expression);
+    }
+};
