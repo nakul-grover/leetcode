52769772ece172d90672fbbf8c8a684795454765
nakul-grover <158311965+nakul-grover@users.noreply.github.com>
2025-01-16 23:43:47 +0530

    Time: 0 ms (100.00%) | Memory: 64.4 MB (36.18%) - LeetSync

--- 2533-bitwise-xor-of-all-pairings/bitwise-xor-of-all-pairings.cpp
+++ 2533-bitwise-xor-of-all-pairings/bitwise-xor-of-all-pairings.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
     int xorAllNums(vector<int>& nums1, vector<int>& nums2) {
+        int xor1 = 0;
+        int xor2 = 0;
+
         int len1 = nums1.size();
         int len2 = nums2.size();
 
-        unordered_map<int, long> freq;
-
-        for (int num : nums1) {
-            freq[num] += len2;
-        }
-
-        for (int num : nums2) {
-            freq[num] += len1;
-        }
-
-        int ans = 0;
-        for (auto& [num, count] : freq) {
-            if (count % 2 == 1) {
-                ans ^= num;
+        if (len2 % 2 != 0) {
+            for (int num : nums1) {
+                xor1 ^= num;
             }
         }
 
-        return ans;
+        if (len1 % 2 != 0) {
+            for (int num : nums2) {
+                xor2 ^= num;
+            }
+        }
+
+        return xor1 ^ xor2;
     }
 };
