7ab83f07566434a301975527064080b1a24038bb
nakul-grover <158311965+nakul-grover@users.noreply.github.com>
2025-03-19 10:39:26 +0530

    Time: 7 ms (59.94%) | Memory: 139.8 MB (93.66%) - LeetSync

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
@@ -2,15 +2,9 @@ class Solution {
 public:
     int minOperations(vector<int>& nums) {
         int count = 0;
-        if(nums[0] == 0){
-            nums[0] = 1;
-            nums[1] = nums[1] ^ 1;
-            nums[2] = nums[2] ^ 1;
-            count++;
-        }
-        for(int i=1;i<nums.size()-2;i++){
+        
+        for(int i=0;i<nums.size()-2;i++){
             if(nums[i] == 0){
-                cout<<i<<endl;
                nums[i] = 1;
                nums[i+1] = nums[i+1] ^ 1;
                nums[i+2] = nums[i+2] ^ 1;
