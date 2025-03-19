2e4ac0c7ea094314a82394cba46d811f4a26864d
nakul-grover <158311965+nakul-grover@users.noreply.github.com>
2025-03-19 13:05:04 +0530

    Time: 355 ms (44.70%) | Memory: 185.7 MB (25.58%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,43 +1,41 @@
-#include <vector>
-using namespace std;
-
 class Solution {
 public:
-    void merge(vector<int>& nums, int left, int mid, int right) {
-        int n1 = mid - left + 1;
-        int n2 = right - mid;
+    
+    void merge(vector<int>& nums, int start, int mid, int end) {
+        int n1 = mid - start + 1;
+        int n2 = end - mid;
 
-        vector<int> v1(n1);
-        vector<int> v2(n2);
+        vector<int> nums1(n1);
+        vector<int> nums2(n2);
 
         for (int i = 0; i < n1; i++) {
-            v1[i] = nums[left + i];
+            nums1[i] = nums[start + i];
         }
         for (int i = 0; i < n2; i++) {
-            v2[i] = nums[mid + 1 + i];
+            nums2[i] = nums[mid + 1 + i];
         }
 
-        int i = 0, j = 0, k = left;
+        int i = 0, j = 0, k = start;
 
         while (i < n1 && j < n2) {
-            if (v1[i] <= v2[j]) {
-                nums[k] = v1[i];
+            if (nums1[i] <= nums2[j]) {
+                nums[k] = nums1[i];
                 i++;
             } else {
-                nums[k] = v2[j];
+                nums[k] = nums2[j];
                 j++;
             }
             k++;
         }
 
         while (i < n1) {
-            nums[k] = v1[i];
+            nums[k] = nums1[i];
             i++;
             k++;
         }
 
         while (j < n2) {
-            nums[k] = v2[j];
+            nums[k] = nums2[j];
             j++;
             k++;
         }
