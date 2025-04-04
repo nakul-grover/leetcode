6e891d90c6830c12e843245fe2fc0c295d24e147
nakul-grover <158311965+nakul-grover@users.noreply.github.com>
2025-04-04 10:12:08 +0530

    Time: 378 ms (39.98%) | Memory: 185.6 MB (27.64%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,58 +1,56 @@
 class Solution {
 public:
-    
-    void merge(vector<int>& nums, int start, int mid, int end) {
-        int n1 = mid - start + 1;
+
+   
+    void merge(vector<int>&nums,int start,int mid,int end){
+
+        int n1 = mid-start+1;
         int n2 = end - mid;
 
-        vector<int> nums1(n1);
-        vector<int> nums2(n2);
+        vector<int>nums1(n1);
+        vector<int>nums2(n2);
 
-        for (int i = 0; i < n1; i++) {
-            nums1[i] = nums[start + i];
+
+        for(int i=0;i<n1;i++){
+            nums1[i] = nums[start+i];
         }
-        for (int i = 0; i < n2; i++) {
-            nums2[i] = nums[mid + 1 + i];
+        for(int i=0;i<n2;i++){
+            nums2[i] = nums[mid+1+i];
         }
+        
+        int i = 0;
+        int j = 0;
+        int k = start;
 
-        int i = 0, j = 0, k = start;
-
-        while (i < n1 && j < n2) {
-            if (nums1[i] <= nums2[j]) {
-                nums[k] = nums1[i];
-                i++;
-            } else {
-                nums[k] = nums2[j];
-                j++;
+        while(i<n1 && j<n2){
+            if(nums1[i] >= nums2[j]){
+                nums[k++] = nums2[j++];
             }
-            k++;
+            else{
+                nums[k++] = nums1[i++];
+            }
         }
 
-        while (i < n1) {
-            nums[k] = nums1[i];
-            i++;
-            k++;
+        while(i<n1){
+            nums[k++] = nums1[i++];
         }
 
-        while (j < n2) {
-            nums[k] = nums2[j];
-            j++;
-            k++;
+        while(j<n2){
+            nums[k++] = nums2[j++];
         }
     }
 
-    void mergeSort(vector<int>& nums, int start, int end) {
-        if (start >= end) {
-            return;
+    void mergeSort(vector<int>&nums,int start,int end){
+        if(start<end){
+           int mid = start + (end-start)/2;
+           mergeSort(nums,start,mid);
+           mergeSort(nums,mid+1,end);
+           merge(nums,start,mid,end);
         }
-        int mid = start + (end - start) / 2;
-        mergeSort(nums, start, mid);
-        mergeSort(nums, mid + 1, end);
-        merge(nums, start, mid, end);
+        return;
     }
-
     vector<int> sortArray(vector<int>& nums) {
-        mergeSort(nums, 0, nums.size() - 1);
+        mergeSort(nums,0,nums.size()-1);
         return nums;
     }
 };
